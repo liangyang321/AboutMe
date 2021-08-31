1c65f34201f57f8da9e0cc7ffa7bc6ff30a4f877
unknown <liangyang321@gmail.com>
2021-08-30 20:54:31 -0400

    about me

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <h1>About me </h1>  
+    <h2>Ang Li</h2>
+    </div>
+  );
+}
+
+export default App;
